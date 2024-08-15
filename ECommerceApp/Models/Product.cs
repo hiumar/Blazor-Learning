102f57569043cbd4b15b5ee1b5ff1bbb4b4a2fd1
Umar Faraz <Umarfaraz005@gmail.com>
2024-08-15 10:26:02 +0400

    Add project files.

---
+++ ECommerceApp/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceApp.Models
+{
+    public class Product
+    {
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public decimal Price {get;set;}
+        public bool IsActive { get; set; }
+        public IEnumerable<Product_Prop> ProductProperties {get; set;}
+
+    }
+}
